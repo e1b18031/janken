2d9e5f18d5e1191d60283c854a29278195bc5236
e1b18031 <e1b18031@st.oit.ac.jp>
2020-11-05 22:56:27 +0900

    SELECTでエントリー表示改

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -2,8 +2,11 @@ package oit.is.z0793.kaizi.janken.model;
 
 public class User {
   int id;
+  String name;
   int user_1;
   int user_2;
+  String user_1_hand;
+  String user_2_hand;
 
   // Thymeleafでフィールドを扱うためにはgetter/setterが必ず必要
   // vscodeのソースコード右クリック->ソースアクションでsetter/getterを簡単に追加できる
@@ -23,12 +26,36 @@ public class User {
     this.user_2 = user_2;
   }
 
+  public String getUser_1Hand() {
+    return user_1_hand;
+  }
+
+  public void setUser_1Hand(String user_1_hand) {
+    this.user_1_hand = user_1_hand;
+  }
+
+  public String getUser_2Hand() {
+    return user_2_hand;
+  }
+
+  public void setUser_2Hand(String user_2_hand) {
+    this.user_2_hand = user_2_hand;
+  }
+
   public int getId() {
     return id;
   }
 
   public void setId(int id) {
     this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
   }
 
 }
